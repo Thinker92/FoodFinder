b8d9f81e550b9f2308bbf55c74a5d3fc48d04204
Vilson Apostolovski II <vilsonapo@gmail.com>
2023-07-26 20:14:10 -0400

    Updated API to render results to the page

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=pasta&cuisine=italian&instructionsRequired=true&number=25';
+const inputEl = document.getElementById("userInput");
+const searchButton = document.getElementById("searchButton");
+
+
 const options = {
     method: 'GET',
     headers: {
@@ -6,17 +9,51 @@ const options = {
         'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
     }
 };
-
-fetch(url, options)
+searchButton.addEventListener('click', function(){
+    const userInput = encodeURIComponent(inputEl.value.trim())
+    const recipeListUrl = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${userInput}&instructionsRequired=true&number=25`;
+    fetch(recipeListUrl, options)
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        return response.text();
+        return response.json();
     })
     .then(result => {
         console.log(result);
+        const recipeList = document.getElementById("recipeResults");
+        result.results.forEach(recipe => {
+            const recipeDiv = document.createElement('div');
+            
+            recipeDiv.innerHTML = `
+                <h2>${recipe.title}</h2>
+                <img src="${recipe.image}" alt="${recipe.title}">
+            `;
+            recipeList.appendChild(recipeDiv);
+        })
     })
     .catch(error => {
         console.error(error);
     });
+})
+
+
+
+
+// let recipeID;
+// const recipeInfoUrl = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeID}/information`;
+
+
+// fetch(recipeInfoUrl, options)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+//         return response.text();
+//     })
+//     .then(result => {
+//         console.log(result);
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
