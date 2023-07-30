35eb674dfe788066efde21a4ed956e63a6970c30
Vilson Apostolovski II <vilsonapo@gmail.com>
2023-07-30 15:29:28 -0400

    Display Recipe when image/title clicked on

--- script.js
+++ script.js
@@ -70,10 +70,11 @@ function displayResults(results) {
 }
 
 function addRecipeToLocalStorage(recipe) {
-    let allRecipes = JSON.parse(localStorage.getItem("savedRecipes")) || [];
-    allRecipes.push(recipe);
+    let allRecipes = JSON.parse(localStorage.getItem("savedRecipes")) || {};
+    allRecipes[recipe.id] = recipe;
     localStorage.setItem("savedRecipes", JSON.stringify(allRecipes));
 }
+
 
 function loadRecipesFromLocal() {
     let allRecipes = JSON.parse(localStorage.getItem("savedRecipes")) || [];
@@ -82,6 +83,7 @@ function loadRecipesFromLocal() {
 
 function fetchRecipeDetails(recipeID) {
     const recipeInfoUrl = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeID}/analyzedInstructions?stepBreakdown=true'`;
+    const recipeDetailUrl = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeID}/information`;
 
     fetch(recipeInfoUrl, options)
         .then(response => {
@@ -92,16 +94,57 @@ function fetchRecipeDetails(recipeID) {
         })
         .then(result => {
             console.log(result);
+            // Clearing Old details
+            document.getElementById("ingredientsList").innerHTML = '';
+            document.getElementById("stepsList").innerHTML = '';
+
+            // Retrieve the saved recipe from localStorage
+            let allRecipes = JSON.parse(localStorage.getItem("savedRecipes")) || {};
+            let recipe = allRecipes[recipeID];
+
+            if (!recipe) {
+                fetch(recipeDetailUrl, options)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(`HTTP error! status: ${response.status}`);
+                        }
+                        return response.json();
+                    })
+                    .then(recipeDetail => {
+                        document.getElementById("recipeTitle").innerText = recipeDetail.title;
+                        document.getElementById("recipeImage").src = recipeDetail.image;
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+            } else {
+                document.getElementById("recipeTitle").innerText = recipe.title;
+                document.getElementById("recipeImage").src = recipe.image;
+            }
+
             result.forEach(item => {
                 item.steps.forEach(step => {
-                    console.log(`Step Text: ${step.step}`)
+                    // Adding steps to modal
+                    let liStep = document.createElement("li");
+                    liStep.innerText = step.step;
+                    document.getElementById("stepsList").appendChild(liStep);
+
                     step.ingredients.forEach(item => {
-                        console.log(`Ingredient: ${item.name}`);
+                        // Adding ingredients to modal
+                        let liIngredient = document.createElement("li");
+                        liIngredient.innerText = item.name;
+                        document.getElementById("ingredientsList").appendChild(liIngredient);
                     })
                 })
             })
+            document.getElementById('recipeDetailsModal').classList.add('is-active');
         })
         .catch(error => {
             console.error(error);
         });
 }
+
+
+document.getElementById('closeButton').addEventListener('click', function() {
+    document.getElementById('recipeDetailsModal').classList.remove('is-active');
+});
